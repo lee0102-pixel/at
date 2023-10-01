6cbfe73ad54873f378ae79a250e72a3cc0ee6f3a
lee <470111091@qq.com>
2023-10-01 20:46:54 +0800

    lightning

---
+++ dataset/data_interface.py
@@ -0,0 +1,91 @@
+import inspect
+import importlib
+import pickle as pkl
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS
+from torch.utils.data import DataLoader
+
+class DInterface(pl.LightningDataModule):
+
+    def __init__(self, args, isp, **kwargs):
+        super().__init__()
+        self.args         = args
+        self.isp          = isp
+        self.num_workers  = args.num_workers
+        self.dataset_name = args.dataset_name
+        self.kwargs       = kwargs
+        self.batch_size   = args.batch_size
+        self.load_data_module(args, isp)
+
+    def load_data_module(self):
+        name = self.dataset_name
+        file_name = name.lower()
+        try:
+            self.data_module = getattr(importlib.import_module(
+                '.'+file_name, package='dataset'), name)
+        except:
+            raise ValueError(
+                f'Invalid Dataset File Name or Invalid Class Name data.{file_name}.{name}'
+            )
+    
+    def setup(self, stage: str) -> None:
+        if stage == 'fit' or stage is None:
+            self.args.is_train = True
+            self.args.data_txt = self.args.train_txt
+            self.train_dataset = self.data_module(self.args, self.isp, **self.kwargs)
+            self.args.is_train = False
+            self.args.data_txt = self.args.val_txt
+            self.val_dataset   = self.data_module(self.args, self.isp, **self.kwargs)
+        if stage == 'test' or stage is None:
+            self.args.is_train = False
+            self.args.data_txt = self.args.val_txt
+            self.test_dataset  = self.data_module(self.args, self.isp, **self.kwargs)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+    
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+    
+
+if __name__ == '__main__':
+    import os
+    import sys
+    #add dir
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(dir_name,'..'))
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--opt', type=str, default='./options/unet_step1.yaml')
+    args = parser.parse_args()
+    from tools.utils import parse_opt
+    parse_opt(args)
+    from isp.ispparams import ISPParams
+    isp_params = ISPParams(args)
+
+    dinterface = DInterface(args, isp_params)
+
+    dinterface.setup('fit')
+    train_loader = dinterface.train_dataloader()
+    print(len(train_loader))
+    for data in train_loader:
+        input_img, gt_img, params, base_name = data
+        print(input_img.shape)
+        print(gt_img.shape)
+        print(params.shape)
+        print(base_name)
+        break
+    dinterface.setup('test')
+    test_loader = dinterface.test_dataloader()
+    print(len(test_loader))
+    for data in test_loader:
+        input_img, gt_img, params, base_name = data
+        print(input_img.shape)
+        print(gt_img.shape)
+        print(params.shape)
+        print(base_name)
+        break
---
+++ models/model_interface.py
@@ -0,0 +1,114 @@
+import os
+import sys
+
+dir_name = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(dir_name,'..'))
+
+from typing import Any, Optional
+from pytorch_lightning.utilities.types import STEP_OUTPUT
+import torch
+import importlib
+import torch.optim.lr_scheduler as lrs
+import pytorch_lightning as pl
+import tools
+import losses.losses as losses
+
+class MInterface(pl.LightningModule):
+    def __init__(self, args, isp, **kwargs):
+        super().__init__()
+        self.args = args
+        self.isp = isp
+        self.kwargs = kwargs
+        self.load_model()
+        self.configure_loss()
+
+    def forward(self, x, params_batch=None):
+        return self.model(x, params_batch)
+    
+    def training_step(self, batch, batch_idx):
+        x, y, params_batch, _ = batch
+        y_hat = self.forward(x, params_batch)
+        loss, loss_dict = self.loss_function(y_hat, y)
+        self.log_dict(loss_dict, on_step=True, on_epoch=True, prog_bar=True)
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y, params_batch, picname = batch
+        y_hat = self.forward(x, params_batch)
+        loss, loss_dict = self.loss_function(y_hat, y)
+
+        self.log_dict(loss_dict, on_step=False, on_epoch=True, prog_bar=True)
+        psnr = tools.get_psnr(y_hat.detach().cpu().numpy(), y.detach().cpu().numpy(), peak=1.0)
+        self.log('psnr', psnr, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {'val_loss': loss, 'psnr': psnr}
+    
+    def test_step(self, batch, batch_idx):
+        return self.validation_step(batch, batch_idx)
+    
+    def on_validation_epoch_end(self) -> None:
+        self.print('')
+
+    def configure_optimizers(self):
+        if self.args.optimizer == 'adam':
+            optimizer = torch.optim.Adam(self.parameters(), 
+                                         lr=self.args.init_lr, 
+                                         betas=(0.9, 0.999), 
+                                         eps=1e-8, 
+                                         weight_decay=self.args.weight_decay)
+        elif self.optimizer == 'adamw':
+            optimizer = torch.optim.AdamW(self.parameters(), 
+                                          lr=self.args.init_lr, 
+                                          betas=(0.9, 0.999), 
+                                          eps=1e-8, 
+                                          weight_decay=self.args.weight_decay)
+        else:
+            raise Exception("Optimizer error!")
+    
+        if self.args.scheduler == 'step':
+            scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 
+                                                        step_size=self.args.step_size, 
+                                                        gamma=self.args.gamma)
+        elif self.args.scheduler == 'cosine':
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
+                                                                   T_max=self.args.T_max, 
+                                                                   eta_min=self.args.eta_min)
+        else:
+            raise Exception("Scheduler error!")
+        
+        return [optimizer], [scheduler]
+    
+    def configure_loss(self):
+        self.loss_function = losses.MixLoss(args=self.args)
+
+    def load_model(self):
+        net_type = self.args.net_type
+        try:
+            module = getattr(importlib.import_module(
+                '.'+net_type, package='models'), net_type)
+        except:
+            raise ValueError(
+                f'Invalid Network Type or Invalid Class Name models.{net_type}.{net_type}'
+            )
+        self.model = module(self.args, self.isp, **self.kwargs)
+
+if __name__ == '__main__':
+    import os
+    import sys
+    #add dir
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(dir_name,'..'))
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--opt', type=str, default='./options/unet_step1.yaml')
+    args = parser.parse_args()
+    from tools.utils import parse_opt
+    parse_opt(args)
+    from isp.ispparams import ISPParams
+    isp_params = ISPParams(args)
+
+    net = MInterface(args, isp_params)
+
+    print(net)
